63d68c228487ae61de2b9f7090a81e88b9db8913
kuvasney <kuvasney@gmail.com>
2026-02-02 17:28:19 -0300

    feat: novas cores tailwind + componente icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,36 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'white-00': '#FFFFFF',
-        'blue-00': '#1C1930',
-        'blue-01': '#1F1283',
-        'blue-02': '#695CCD',
-        'gray-00': '#717171',
-        'gray-01': '#F4F2FF',
+        "white-00": "#FFFFFF",
+        "blue-00": "#1C1930",
+        "blue-01": "#1F1283",
+        "blue-02": "#695CCD",
+        "gray-00": "#717171",
+        "gray-01": "#F4F2FF",
+        "gray-02": "#1E1E1E",
+        "gray-03": "#DCDCDC",
+        "gray-04": "#53525D",
+        "gray-05": "#ECECEC",
+        "yellow-00": "#FFB23D",
       },
       height: {
-        header: '80px',
+        header: "80px",
       },
       boxShadow: {
-        'header': '0px 4px 4px 0px #00000040',
+        header: "0px 4px 4px 0px #00000040",
+        default: "0px 4px 4px 0px #00000040",
+      },
+      dropShadow: {
+        text: "0px 0px 4px rgba(0, 0, 0, 0.5)",
       },
       fontFamily: {
-        sans: ['"Encode Sans Semi Expanded"', 'sans-serif'],
+        sans: ['"Encode Sans Semi Expanded"', "sans-serif"],
       },
     },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
