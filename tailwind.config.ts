eb43ab4590bfd017f6e602f63323d83e84b9b001
kuvasney <kuvasney@gmail.com>
2026-02-02 09:54:41 -0300

    feat: Tailwind + CSS para integrações

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blue-00': '#1C1930',
+        'blue-01': '#1F1283',
+        'blue-02': '#695CCD',
+        'gray-00': '#717171',
+        'gray-01': '#F4F2FF',
+      },
+      height: {
+        header: '80px',
+      },
+      boxShadow: {
+        'header': '0px 4px 4px 0px #00000040',
+      },
+      fontFamily: {
+        sans: ['"Encode Sans Semi Expanded"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
